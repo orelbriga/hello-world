8dda63ea355eb5b091360559f0491dc5a5af8d07
Orel Briga <orelbriga@gmail.com>
2022-09-06 22:16:24 +0300

    changing input for NODE_PORT

--- vars/testCall.groovy
+++ vars/testCall.groovy
@@ -1,3 +1,10 @@
 def call() {
-    echo "testing call method.."
+    sh(
+            script: './kubectl get pod | grep hello-world-app-$BUILD_NUMBER-* | \
+                                  awk \'{print $1}\'',
+            returnStdout: true
+    ).trim()
 }
+
+
+// sh script: "cp -r ~/.gradle /jenkinsPv/gradle/${env.JOB_NAME}/"
