615472b5b0cb8f055ac0ad842dbdfaf561091527
Orel Briga <orelbriga@gmail.com>
2022-09-24 15:16:33 +0300

    fix spring context test

--- src/test/java/com/spring/helloworld/HelloWorldApplicationTests.java
+++ src/test/java/com/spring/helloworld/HelloWorldApplicationTests.java
@@ -2,12 +2,16 @@ package com.spring.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		assertThat(context).isNotNull();
+		// test that would fail if our Spring configuration does not load correctly
 	}
-
 }
